7cb531d1b09efb9f23b9e6df58c2a77d15ab268f
JHONNYNERO <jhonnynereos@gmail.com>
2018-06-11 19:22:48 -0500

    Added functionality to add and show abilities

--- Portafolio.Modelo/Habilidad.cs
+++ Portafolio.Modelo/Habilidad.cs
@@ -4,7 +4,9 @@ namespace Portafolio.Modelo
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Habilidad")]
     public partial class Habilidad
@@ -17,8 +19,99 @@ namespace Portafolio.Modelo
         [StringLength(50)]
         public string Nombre { get; set; }
 
+        [Range(1, 100, ErrorMessage ="Debe ingresar un valor entre 1 y 100")]
         public int Dominio { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Habilidad Obtener(int id)
+        {
+            var habilidad = new Habilidad();
+
+            try
+            {
+                using (var ctx = new PortafolioModel())
+                {
+                    habilidad = ctx.Habilidads.Where(x => x.id == id).FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return habilidad;
+        }
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new PortafolioModel())
+                {
+                    var Habilidad = ctx.Habilidads.Where(x => x.id == id).FirstOrDefault();
+                    var entry = ctx.Entry(Habilidad);
+                    entry.State = EntityState.Deleted;
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return rm;
+        }
+
+        public List<Habilidad> ObtenerLista(int id_usuario)
+        {
+            var Habilidad = new List<Habilidad>();
+
+            try
+            {
+                using (var ctx = new PortafolioModel())
+                {
+                    Habilidad = ctx.Habilidads.Where(x => x.Usuario_id == id_usuario)
+                                              .ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Habilidad;
+        }
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new PortafolioModel())
+                {
+                    var Habilidad = ctx.Entry(this);
+                    if (this.id > 0)
+                    {
+                        Habilidad.State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        Habilidad.State = EntityState.Added;
+                    }
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return rm;
+
+        }
     }
 }
---
+++ Portafolio.Web/Areas/Admin/Controllers/HabilidadesController.cs
@@ -0,0 +1,76 @@
+﻿using Portafolio.Helper;
+using Portafolio.Modelo;
+using Portafolio.Web.Areas.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portafolio.Web.Areas.Admin.Controllers
+{
+    [Autenticado]
+    public class HabilidadesController : Controller
+    {
+        private Habilidad habilidad = new Habilidad();
+
+        public ActionResult Index()
+        {
+            ViewBag.Habilidades= habilidad.ObtenerLista(SessionHelper.GetUser());
+            return View(habilidad);
+        }
+
+        public ActionResult Crud(int id = 0)
+        {
+            if (id>0)
+            {
+                habilidad = habilidad.Obtener(id);
+            }            
+            return View(habilidad);
+        }
+
+        public JsonResult Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    rm = habilidad.Eliminar(id);
+                    if (rm.response)
+                    {
+                        rm.href = "/admin/habilidades/";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(rm);
+        }
+
+        public JsonResult Guardar(Habilidad habilid)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    rm = habilid.Guardar();
+                    if (rm.response)
+                    {
+                        rm.href = "/admin/habilidades/";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(rm);
+        }
+    }
+}
